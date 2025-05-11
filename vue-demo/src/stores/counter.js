60861d6f21930790bd71ff0c54af2ae5afd90f39
cy2334 <1178849864@qq.com>
2025-05-11 22:53:26 +0800

    状态管理Pinia

--- vue-demo/src/stores/counter.js
+++ vue-demo/src/stores/counter.js
@@ -1,1 +1,22 @@
 import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore('counter',{
+    state:()=>{
+        return{
+            count:0,
+        };
+    },
+    actions:{
+        increment(){
+            this.count++;
+        },
+        decrement(){
+            this.count--;
+        }
+    },
+    getters:{
+        doubleCount(state){
+            return state.count * 2;
+        },
+    },
+})
---
+++ vue-demo/src/views/StateManagement.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <p>计数器: {{ counter.count }}</p>
+    <p>双倍计数：{{ counter.doubleCount }}</p>
+    <button @click="counter.increment">+1</button>
+    <button @click="counter.decrement">-1</button>
+  </div>
+</template>
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+const counter = useCounterStore();
+</script>
